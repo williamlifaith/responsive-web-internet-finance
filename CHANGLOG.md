09f5e543548b3593d62bdbbccc0f582826d9c134
JosephLi <williamlifaith@gmail.com>
2018-01-10 20:14:16 -0600

    changed the js link

---
+++ CHANGLOG.md
@@ -0,0 +1,4 @@
+### 1.0
+
+* fix n bugs
+* add n featues
---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
+
